a72c3d7ef8dc46486c26a908becd64747196e375
Полина <123145524+polshtva@users.noreply.github.com>
2023-08-07 18:10:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// let arrNuv = document.querySelectorAll('.nav__link')
+// console.log(arrNuv[1]);
+// for(let i = 1;  i < arrNuv.length; i++){
+//         arrNuv[i].addEventListener('mouseover',  val =>
+//             console.log(arrNuv.indexOf(val))
+//     )
+// }
+
+// // Ширина меню 1104 / 837.25
+
+
+// Задачи
+
+// let str = 'abcdefg';
+// let strArr=[]
+// for(let i in str){
+//     if(i % 3 == 0){
+//        strArr.push[i]; 
+//     }
+// }
+// console.log(strArr);
+
+
+
+//2232
